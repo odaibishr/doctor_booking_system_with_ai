61fd4f51e45b11d00bc7593cbdd87b292f179f5d
Odai Bishr <odaibishr@gmail.com>
2025-10-09 20:05:12 +0300

    add the font styles

---
+++ lib/core/styles/font_styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FontStyles {
+  // headLines
+  static const TextStyle headLine1 = TextStyle(fontSize: 40, height: 1.30);
+  static const TextStyle headLine2 = TextStyle(fontSize: 36, height: 1.30);
+  static const TextStyle headLine3 = TextStyle(fontSize: 32, height: 1.30);
+  static const TextStyle headLine4 = TextStyle(fontSize: 20, height: 1.30);
+
+  // subtitles
+  static const TextStyle subTitle1 = TextStyle(fontSize: 18, height: 1.30);
+  static const TextStyle subTitle2 = TextStyle(fontSize: 16, height: 1.30);
+  static const TextStyle subTitle3 = TextStyle(fontSize: 14, height: 1.30);
+
+  // body
+  static const TextStyle body1 = TextStyle(fontSize: 13, height: 1.30);
+  static const TextStyle body2 = TextStyle(fontSize: 12, height: 1.30);
+  static const TextStyle body3 = TextStyle(fontSize: 10, height: 1.30);
+  static const TextStyle body4 = TextStyle(fontSize: 8, height: 1.30);
+}
