69cb42bc7070da97f752782f2745624807f1a653
Odai Bishr <odaibishr@gmail.com>
2025-10-12 23:10:26 +0300

    creat main input field

---
+++ lib/core/widgets/main_input_field.dart
@@ -0,0 +1,84 @@
+import 'package:doctor_booking_system_with_ai/core/styles/app_colors.dart';
+import 'package:doctor_booking_system_with_ai/core/styles/font_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:svg_flutter/svg.dart';
+
+class MainInputField extends StatelessWidget {
+  const MainInputField({
+    super.key,
+    required this.hintText,
+    required this.leftIconPath,
+    required this.rightIconPath,
+    required this.isShowRightIcon,
+    required this.isShowLeftIcon,
+  });
+  final String hintText;
+  final String leftIconPath;
+  final String rightIconPath;
+  final bool isShowRightIcon;
+  final bool isShowLeftIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 45,
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        border: Border.all(color: AppColors.gray400),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          isShowRightIcon
+              ? SvgPicture.asset(
+                  leftIconPath,
+                  width: 20,
+                  height: 20,
+                  fit: BoxFit.scaleDown,
+                  colorFilter: ColorFilter.mode(
+                    AppColors.gray400,
+                    BlendMode.srcIn,
+                  ),
+                )
+              : const SizedBox.shrink(),
+
+          const SizedBox(width: 5),
+          Expanded(
+            child: TextField(
+              textAlignVertical: TextAlignVertical.center,
+              cursorColor: AppColors.gray400,
+              decoration: InputDecoration(
+                isCollapsed: true,
+                border: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                errorBorder: InputBorder.none,
+                disabledBorder: InputBorder.none,
+                contentPadding: EdgeInsets.zero,
+                hintText: hintText,
+                hintStyle: FontStyles.subTitle2.copyWith(
+                  color: AppColors.gray400,
+                ),
+              ),
+            ),
+          ),
+
+          isShowLeftIcon
+              ? SvgPicture.asset(
+                  rightIconPath,
+                  width: 20,
+                  height: 20,
+                  fit: BoxFit.scaleDown,
+                  colorFilter: ColorFilter.mode(
+                    AppColors.gray400,
+                    BlendMode.srcIn,
+                  ),
+                )
+              : const SizedBox.shrink(),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/search/presentation/widgets/search_view_body.dart
+++ lib/features/search/presentation/widgets/search_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:doctor_booking_system_with_ai/core/widgets/custom_app_bar.dart';
+import 'package:doctor_booking_system_with_ai/core/widgets/main_input_field.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -14,11 +15,18 @@ class SearchViewBody extends StatelessWidget {
             children: [
               CustomAppBar(
                 userImage: 'assets/images/my-photo.jpg',
-                title: 'بحث',
+                title: 'كل الأطباء',
                 isBackButtonVisible: true,
                 isUserImageVisible: true,
               ),
               const SizedBox(height: 16),
+              const MainInputField(
+                hintText: 'بحث',
+                leftIconPath: 'assets/icons/search.svg',
+                rightIconPath: 'assets/icons/user.svg',
+                isShowRightIcon: true,
+                isShowLeftIcon: false,
+              ),
             ],
           ),
         ),
