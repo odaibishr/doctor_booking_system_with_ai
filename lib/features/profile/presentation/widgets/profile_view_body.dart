38cbb3680384c5b82662ba9d182dd77f699ae0d4
Odai Bishr <odaibishr@gmail.com>
2025-10-15 23:28:53 +0300

    create user account menu and its items

--- lib/features/profile/presentation/widgets/profile_view_body.dart
+++ lib/features/profile/presentation/widgets/profile_view_body.dart
@@ -1,7 +1,6 @@
-import 'package:doctor_booking_system_with_ai/core/styles/app_colors.dart';
-import 'package:doctor_booking_system_with_ai/core/styles/font_styles.dart';
 import 'package:doctor_booking_system_with_ai/core/widgets/custom_app_bar.dart';
 import 'package:doctor_booking_system_with_ai/features/profile/presentation/widgets/profile_summary.dart';
+import 'package:doctor_booking_system_with_ai/features/profile/presentation/widgets/user_account_menu.dart';
 import 'package:flutter/material.dart';
 
 class ProfileViewBody extends StatelessWidget {
@@ -9,26 +8,41 @@ class ProfileViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(height: 10),
-          CustomAppBar(
+    return CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: [
+        SliverAppBar(
+          title: CustomAppBar(
             userImage: 'assets/images/my-photo.jpg',
             title: 'الحساب الشخصي',
             isBackButtonVisible: false,
             isUserImageVisible: false,
           ),
-          const SizedBox(height: 30),
-          ProfileSummary(
-            name: 'عدي بشر',
-            userImage: 'assets/images/my-photo.jpg',
-            phoneNumber: '967774536599+',
+          pinned: true,
+          backgroundColor: Colors.white,
+          surfaceTintColor: Colors.white,
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 10),
+
+                const SizedBox(height: 30),
+                ProfileSummary(
+                  name: 'عدي بشر',
+                  userImage: 'assets/images/my-photo.jpg',
+                  phoneNumber: '967774536599+',
+                ),
+                const SizedBox(height: 30),
+                const UserAccountMenu(),
+              ],
+            ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
