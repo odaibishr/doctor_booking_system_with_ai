7ef4370bdf5344af9914f0f1be765ef1bb3b086f
Odai Bishr <odaibishr@gmail.com>
2025-10-17 21:01:02 +0300

    add the docotr description

--- lib/features/home/presentation/widgets/details_view_body.dart
+++ lib/features/home/presentation/widgets/details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:doctor_booking_system_with_ai/core/styles/app_colors.dart';
+import 'package:doctor_booking_system_with_ai/core/styles/font_styles.dart';
 import 'package:doctor_booking_system_with_ai/core/widgets/custom_app_bar.dart';
 import 'package:doctor_booking_system_with_ai/features/home/presentation/widgets/details/doctor_header_section.dart';
 import 'package:doctor_booking_system_with_ai/features/home/presentation/widgets/details/doctor_stats_section.dart';
@@ -11,6 +13,7 @@ class DetailsViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomAppBar(
             userImage: 'assets/images/user.png',
@@ -28,6 +31,18 @@ class DetailsViewBody extends StatelessWidget {
           ),
           const SizedBox(height: 16),
           DoctorStatsSection(),
+          const SizedBox(height: 22),
+
+          // Doctor Description
+          Text(
+            'نبذة عن الدكتور',
+            style: FontStyles.subTitle2.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 7),
+          Text(
+            'دكتور متخصص حاصل على الدكتوراه والبورد الأردني في تخصصه، بعد تدريب مكثف في العراق والأردن. تخرج في الطب العام من روسيا، وتمتع بخبرة عملية في مستشفى الثورة بتعز، والمستشفى الألماني بتعز، بالإضافة إلى عمله في المملكة العربية السعودية.',
+            style: FontStyles.body2.copyWith(color: AppColors.gray500),
+          ),
         ],
       ),
     );
