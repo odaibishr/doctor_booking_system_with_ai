a61c2e850c9a66de7f030d27e0d27d2e0d7ec701
Odai Bishr <odaibishr@gmail.com>
2025-10-22 00:04:38 +0300

    add app bar to hospital details

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -2,6 +2,7 @@ import 'package:doctor_booking_system_with_ai/features/app/presentation/app_navi
 import 'package:doctor_booking_system_with_ai/features/booking_history/presentation/booking_history_view.dart';
 import 'package:doctor_booking_system_with_ai/features/home/presentation/details_view.dart';
 import 'package:doctor_booking_system_with_ai/features/home/presentation/home_view.dart';
+import 'package:doctor_booking_system_with_ai/features/hospital/presentation/hospital_details_view.dart';
 import 'package:doctor_booking_system_with_ai/features/profile/presentation/profile_view.dart';
 import 'package:doctor_booking_system_with_ai/features/search/presentation/search_view.dart';
 import 'package:doctor_booking_system_with_ai/features/splash/presentation/splash_view.dart';
@@ -15,6 +16,7 @@ class AppRouter {
   static const String bookingHistoryViewRoute = '/bookingHistoryView';
   static const String profileViewRoute = '/profileView';
   static const String detailsViewRoute = '/detailsView';
+  static const String hospitalDetailsViewRoute = '/hospitalDetailsView';
 
   static GoRouter router = GoRouter(
     routes: [
@@ -45,6 +47,10 @@ class AppRouter {
       GoRoute(
         path: detailsViewRoute,
         builder: (context, state) => const DetailsView(),
+      ),
+      GoRoute(
+        path: hospitalDetailsViewRoute,
+        builder: (context, state) => const HospitalDetailsView(),
       ),
     ],
     initialLocation: splashRoute,
--- lib/features/hospital/presentation/widgets/hospital_details_view_body.dart
+++ lib/features/hospital/presentation/widgets/hospital_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_booking_system_with_ai/core/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class HospitalDetailsViewBody extends StatelessWidget {
@@ -5,9 +6,18 @@ class HospitalDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-        
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      child: Column(
+        children: [
+          CustomAppBar(
+            title: 'معلومات المستشفى',
+            isBackButtonVisible: true,
+            isUserImageVisible: false,
+            isHeartIconVisible: false,
+          ),
+        ],
+      ),
     );
   }
 }
