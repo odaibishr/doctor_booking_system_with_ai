436fe3b9f1510419a14a578dcafaa2aefd37ae7d
Odai Bishr <odaibishr@gmail.com>
2025-10-21 23:54:27 +0300

    create hospital details view and body

---
+++ lib/features/hospital/presentation/widgets/hospital_details_view_body.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HospitalDetailsViewBody extends StatelessWidget {
+  const HospitalDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+        
+      ],
+    );
+  }
+}
