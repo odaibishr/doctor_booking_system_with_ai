a7d26d7bef47dd6b96314789b3dcf91f17d2a4b8
Odai Bishr <odaibishr@gmail.com>
2025-10-22 00:19:33 +0300

    create hospital header section

--- lib/features/hospital/presentation/widgets/hospital_details_view_body.dart
+++ lib/features/hospital/presentation/widgets/hospital_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:doctor_booking_system_with_ai/core/widgets/custom_app_bar.dart';
+import 'package:doctor_booking_system_with_ai/features/hospital/presentation/widgets/hospital_header_section.dart';
 import 'package:flutter/material.dart';
 
 class HospitalDetailsViewBody extends StatelessWidget {
@@ -9,6 +10,7 @@ class HospitalDetailsViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomAppBar(
             title: 'معلومات المستشفى',
@@ -16,6 +18,13 @@ class HospitalDetailsViewBody extends StatelessWidget {
             isUserImageVisible: false,
             isHeartIconVisible: false,
           ),
+          const SizedBox(height: 28),
+          HospitalHeaderSection(
+            hospitalName: 'مستشفى جامعة العلوم والتكنولوجيا',
+            hospitalLocation: 'صنعاء . شارع الستين . جسر مذبح',
+            hospitalImage: 'assets/images/hospital.jpg',
+          ),
+          const SizedBox(height: 20),
         ],
       ),
     );
