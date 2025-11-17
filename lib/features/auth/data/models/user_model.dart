207963617b929d517b75fce863ff749d242c8c9b
Odai Bishr <odaibishr@gmail.com>
2025-11-17 20:29:44 +0300

    refactor user model and make it extends from user entity

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -1,28 +1,34 @@
-class UserModel {
-  final String name;
-  final String email;
-  final String password;
+import 'package:doctor_booking_system_with_ai/features/auth/domain/entities/user.dart';
 
-  UserModel({required this.name, required this.email, required this.password});
+class UserModel extends User {
+  const UserModel({
+    required super.id,
+    required super.name,
+    required super.email,
+    required super.password,
+  });
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
-      name: json['name'],
-      email: json['email'],
-      password: json['password'],
+      id: json['id'] as String,
+      name: json['name'] as String,
+      email: json['email'] as String,
+      password: json['password'] as String,
     );
   }
 
   Map<String, dynamic> toJson() {
-    return {
-      'name': name,
-      'email': email,
-      'password': password,
-    };
+    return {'id': id, 'name': name, 'email': email, 'password': password};
   }
 
-  UserModel copyWith({String? name, String? email, String? password}) {
+  UserModel copyWith({
+    String? id,
+    String? name,
+    String? email,
+    String? password,
+  }) {
     return UserModel(
+      id: id ?? this.id,
       name: name ?? this.name,
       email: email ?? this.email,
       password: password ?? this.password,
