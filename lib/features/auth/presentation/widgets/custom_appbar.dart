17b43ed5e3324cdaa942a059f1f7e621e82d9e45
Odai Bishr <odaibishr@gmail.com>
2025-11-23 20:42:07 +0300

    edit sign up app bar

--- lib/features/auth/presentation/widgets/custom_appbar.dart
+++ lib/features/auth/presentation/widgets/custom_appbar.dart
@@ -2,9 +2,7 @@ import 'package:doctor_booking_system_with_ai/core/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class AppBar2 extends StatelessWidget {
-  const AppBar2({
-    super.key,
-  });
+  const AppBar2({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +13,7 @@ class AppBar2 extends StatelessWidget {
         isUserImageVisible: false,
       ),
       pinned: true,
+      automaticallyImplyLeading: false,
     );
   }
 }
