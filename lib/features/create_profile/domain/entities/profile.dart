bbf0f011b4d91184eeecae01130edb826f9955bb
Odai Bishr <odaibishr@gmail.com>
2025-11-23 22:45:38 +0300

    create profile entity

---
+++ lib/features/create_profile/domain/entities/profile.dart
@@ -0,0 +1,17 @@
+class Profile {
+  final int id;
+  final String phone;
+  final String birthDate;
+  final String gender;
+  final int locationId;
+  final int userId;
+
+  Profile({
+    required this.id,
+    required this.phone,
+    required this.birthDate,
+    required this.gender,
+    required this.locationId,
+    required this.userId,
+  });
+}
