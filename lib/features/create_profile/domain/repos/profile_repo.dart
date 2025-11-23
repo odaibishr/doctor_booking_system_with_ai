c856050a27f40728c962ed161f1ec446643ac1b6
Odai Bishr <odaibishr@gmail.com>
2025-11-23 22:48:06 +0300

    add porfile repo

---
+++ lib/features/create_profile/domain/repos/profile_repo.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:doctor_booking_system_with_ai/core/errors/failure.dart';
+import 'package:doctor_booking_system_with_ai/features/create_profile/domain/entities/profile.dart';
+
+abstract class ProfileRepo {
+  Future<Either<Failure, Profile>> createProfile({
+    required String phone,
+    required String birthDate,
+    required String gender,
+    required int locationId,
+  });
+}
