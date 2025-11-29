9a52443078bf30b3e2135b1134dc72e2b8949b50
Odai Bishr <odaibishr@gmail.com>
2025-11-29 19:58:53 +0300

    add to json and from json to profile model

--- lib/features/create_profile/data/datasources/profile_remote_data_source.dart
+++ lib/features/create_profile/data/datasources/profile_remote_data_source.dart
@@ -1,3 +1,7 @@
+import 'dart:developer';
+
+import 'package:doctor_booking_system_with_ai/core/database/api/dio_consumer.dart';
+import 'package:doctor_booking_system_with_ai/features/create_profile/data/models/porfile_model.dart';
 import 'package:doctor_booking_system_with_ai/features/create_profile/domain/entities/profile.dart';
 
 abstract class ProfileRemoteDataSource {
@@ -9,3 +13,27 @@ abstract class ProfileRemoteDataSource {
   });
 }
 
+class ProfileRemoteDataSourceImpl implements ProfileRemoteDataSource {
+  final DioConsumer dioConsumer;
+  @override
+  Future<Profile> createProfile({
+    required String phone,
+    required String birthDate,
+    required String gender,
+    required int locationId,
+  }) async{
+    final response = await dioConsumer.post(
+      'profile',
+      data: {
+        'phone': phone,
+        'birth_date': birthDate,
+        'gender': gender,
+        'location_id': locationId,
+      }
+    );
+
+    log("Create Profile response: $response");
+
+    return ProfileModel.fromJson(response);    
+  }
+}
