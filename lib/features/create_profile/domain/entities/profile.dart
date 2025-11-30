0eacb5c685be8541845d82ce86aea24c184e57e6
Odai Bishr <odaibishr@gmail.com>
2025-11-30 22:41:04 +0300

    Add profile adapter for hive

--- lib/features/create_profile/domain/entities/profile.dart
+++ lib/features/create_profile/domain/entities/profile.dart
@@ -1,9 +1,17 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:hive_flutter/hive_flutter.dart';
+part 'profile.g.dart';
+
+@HiveType(typeId: 1)
 class Profile {
+  @HiveField(0)
   final String phone;
+  @HiveField(1)
   final String birthDate;
+  @HiveField(2)
   final String gender;
+  @HiveField(3)
   final int locationId;
+  @HiveField(4)
   final String? profileImage;
 
   Profile({
---
+++ lib/features/create_profile/domain/entities/profile.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ProfileAdapter extends TypeAdapter<Profile> {
+  @override
+  final int typeId = 1;
+
+  @override
+  Profile read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Profile(
+      phone: fields[0] as String,
+      birthDate: fields[1] as String,
+      gender: fields[2] as String,
+      locationId: fields[3] as int,
+      profileImage: fields[4] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Profile obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.phone)
+      ..writeByte(1)
+      ..write(obj.birthDate)
+      ..writeByte(2)
+      ..write(obj.gender)
+      ..writeByte(3)
+      ..write(obj.locationId)
+      ..writeByte(4)
+      ..write(obj.profileImage);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ProfileAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
