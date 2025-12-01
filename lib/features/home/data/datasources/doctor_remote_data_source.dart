d06a0090ab08e505544d4317b0e8bed54b6c000c
Odai Bishr <odaibishr@gmail.com>
2025-12-01 18:59:43 +0300

    create doctor remote data source and implement get doctors

--- lib/features/home/data/datasources/doctor_remote_data_source.dart
+++ lib/features/home/data/datasources/doctor_remote_data_source.dart
@@ -1,5 +1,28 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:doctor_booking_system_with_ai/core/database/api/dio_consumer.dart';
+import 'package:doctor_booking_system_with_ai/features/home/data/models/doctor_model.dart';
 import 'package:doctor_booking_system_with_ai/features/home/domain/entities/doctor.dart';
 
 abstract class DoctorRemoteDataSource {
   Future<List<Doctor>> getDoctors();
+}
+
+class DoctorRemoteDataSourceImpl implements DoctorRemoteDataSource {
+  final DioConsumer dioConsumer;
+
+  DoctorRemoteDataSourceImpl(this.dioConsumer);
+
+  @override
+  Future<List<Doctor>> getDoctors() async {
+    log("Fetching doctors from remote data source");
+    final response = await dioConsumer.get('doctor/AllDoctors');
+
+    final doctors = (response['data'] as List)
+        .map((doctorData) => DoctorModel.fromMap(doctorData))
+        .toList();
+    log("Fetched ${doctors.length} doctors from remote data source");
+    return doctors;
+  }
 }
