9b0d13f8647c461718624c67f08bc6e024a4de68
Odai Bishr <odaibishr@gmail.com>
2025-12-01 18:14:30 +0300

    Add Doctor Repo and get doctors function

---
+++ lib/features/home/domain/repos/doctor_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:doctor_booking_system_with_ai/core/errors/failure.dart';
+import 'package:doctor_booking_system_with_ai/features/home/domain/entities/doctor.dart';
+
+abstract class DoctorRepo {
+  Future<Either<Failure, List<Doctor>>> getDoctors();
+  
+}
