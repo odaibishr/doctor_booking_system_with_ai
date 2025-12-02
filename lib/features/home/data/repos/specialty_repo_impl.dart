cf5a1951790ce450cc85eec622798b3272eadff2
Odai Bishr <odaibishr@gmail.com>
2025-12-02 19:44:42 +0300

    Add get specialties to specialty repo impl

--- lib/features/home/data/repos/specialty_repo_impl.dart
+++ lib/features/home/data/repos/specialty_repo_impl.dart
@@ -1,12 +1,26 @@
+import 'dart:developer';
+
 import 'package:dartz/dartz.dart';
 import 'package:doctor_booking_system_with_ai/core/errors/failure.dart';
+import 'package:doctor_booking_system_with_ai/features/home/data/datasources/specilaty_remote_data_source.dart';
 import 'package:doctor_booking_system_with_ai/features/home/domain/entities/specialty.dart';
 import 'package:doctor_booking_system_with_ai/features/home/domain/repos/specialty_repo.dart';
 
 class SpecialtyRepoImpl implements SpecialtyRepo {
+  final SpecilatyRemoteDataSource remoteDataSource;
+
+  SpecialtyRepoImpl(this.remoteDataSource);
   @override
-  Future<Either<Failure, List<Specialty>>> getSpecialties() {
-    // TODO: implement getSpecialties
-    throw UnimplementedError();
+  Future<Either<Failure, List<Specialty>>> getSpecialties() async {
+    try {
+      log("Fetching specialties from remote data source");
+      final result = await remoteDataSource.getSpecialties();
+
+      log("Fetched ${result.length} specialties from remote data source");
+      return Right(result);
+    } catch (error) {
+      log('Error fetching specialties: $error');
+      return Left(Failure(error.toString()));
+    }
   }
 }
