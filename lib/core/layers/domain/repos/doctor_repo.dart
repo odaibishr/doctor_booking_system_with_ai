fe404c9920b08b1bb91561c9c086be1625402902
Odai Bishr <odaibishr@gmail.com>
2025-12-04 17:07:47 +0300

    Add Search Doctors use case

--- lib/core/layers/domain/repos/doctor_repo.dart
+++ lib/core/layers/domain/repos/doctor_repo.dart
@@ -5,4 +5,5 @@ import 'package:doctor_booking_system_with_ai/core/layers/domain/entities/doctor
 abstract class DoctorRepo {
   Future<Either<Failure, List<Doctor>>> getDoctors();
   Future<Either<Failure, Doctor>> getDoctorDetails(int id);
+  Future<Either<Failure, List<Doctor>>> searchDoctors(String query);
 }
