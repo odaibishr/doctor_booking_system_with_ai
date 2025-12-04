8bf67bac65c2ed1313629d318e0755fc1e9a365a
Odai Bishr <odaibishr@gmail.com>
2025-12-04 17:36:13 +0300

    Add search doctor to bloc

--- lib/features/search/presentation/manager/search_doctors_bloc/search_doctors_bloc.dart
+++ lib/features/search/presentation/manager/search_doctors_bloc/search_doctors_bloc.dart
@@ -1,14 +1,37 @@
 import 'package:bloc/bloc.dart';
 import 'package:doctor_booking_system_with_ai/core/layers/domain/entities/doctor.dart';
+import 'package:doctor_booking_system_with_ai/features/search/domain/usecases/search_doctors_use_case.dart';
 import 'package:meta/meta.dart';
 
 part 'search_doctors_event.dart';
 part 'search_doctors_state.dart';
 
 class SearchDoctorsBloc extends Bloc<SearchDoctorsEvent, SearchDoctorsState> {
-  SearchDoctorsBloc() : super(SearchDoctorsInitial()) {
-    on<SearchDoctorsEvent>((event, emit) {
-      // TODO: implement event handler
+  final SearchDoctorsUseCase searchDoctorsUseCase;
+
+  SearchDoctorsBloc(this.searchDoctorsUseCase) : super(SearchDoctorsInitial()) {
+    on<SearchDoctorsQueryChanged>((event, emit) {
+      onSearchDcotrosQueryChanged(event, emit);
     });
+  }
+
+  Future<void> onSearchDcotrosQueryChanged(
+    SearchDoctorsQueryChanged event,
+    Emitter<SearchDoctorsState> emit,
+  ) async {
+    if (event.query.isEmpty) {
+      emit(SearchDoctorsInitial());
+      return;
+    } else {
+      emit(SearchDoctorsLoading());
+
+      final result = await searchDoctorsUseCase(
+        SearchDoctorsUseCaseParams(event.query),
+      );
+      result.fold(
+        (failure) => emit(SearchDoctorsError(failure.errorMessage)),
+        (doctors) => emit(SearchDoctorsLoaded(doctors)),
+      );
+    }
   }
 }
