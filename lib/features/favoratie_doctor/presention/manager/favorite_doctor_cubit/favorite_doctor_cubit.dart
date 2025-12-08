214e65a171f3027bf2b6a0b90996d544dd921c67
Odai Bishr <odaibishr@gmail.com>
2025-12-09 00:03:37 +0300

    Add Favorite doctors trigger cubit

--- lib/features/favoratie_doctor/presention/manager/favorite_doctor_cubit/favorite_doctor_cubit.dart
+++ lib/features/favoratie_doctor/presention/manager/favorite_doctor_cubit/favorite_doctor_cubit.dart
@@ -1,12 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:doctor_booking_system_with_ai/core/layers/domain/entities/doctor.dart';
-import 'package:doctor_booking_system_with_ai/core/layers/domain/usecases/get_doctors_use_case.dart';
+import 'package:doctor_booking_system_with_ai/features/favoratie_doctor/domain/use_cases/get_favorite_doctors_use_case.dart';
 
 part 'favorite_doctor_state.dart';
 
 class FavoriteDoctorCubit extends Cubit<FavoriteDoctorState> {
-  final GetDoctorsUseCase getDoctorsUseCase;
-  FavoriteDoctorCubit(this.getDoctorsUseCase) : super(FavoriteDoctorInitial());
+  final GetFavoriteDoctorsUseCase getFavoriteDoctorsUseCase;
+  FavoriteDoctorCubit(this.getFavoriteDoctorsUseCase)
+    : super(FavoriteDoctorInitial());
 
-  
+  Future<void> getFavoriteDoctors() async {
+    emit(FavoirteDoctorsLoading());
+    try {
+      final result = await getFavoriteDoctorsUseCase();
+
+      result.fold(
+        (failure) => emit(FavoriteDoctorsError(failure.errorMessage)),
+        (doctors) => emit(FavoriteDoctorsLoaded(doctors)),
+      );
+    } catch (error) {
+      emit(FavoriteDoctorsError(error.toString()));
+    }
+  }
 }
