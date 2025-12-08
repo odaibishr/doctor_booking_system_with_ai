4820d44f6d1d4b83e295721a547b3dba76bdc271
Odai Bishr <odaibishr@gmail.com>
2025-12-09 01:44:06 +0300

    fetch hospital data from remote data source

--- lib/core/layers/data/repos/hospital_repo_impl.dart
+++ lib/core/layers/data/repos/hospital_repo_impl.dart
@@ -1,12 +1,32 @@
 import 'package:dartz/dartz.dart';
 import 'package:doctor_booking_system_with_ai/core/errors/failure.dart';
+import 'package:doctor_booking_system_with_ai/core/layers/data/datasources/hospital_remote_data_source.dart';
 import 'package:doctor_booking_system_with_ai/core/layers/domain/entities/hospital.dart';
 import 'package:doctor_booking_system_with_ai/core/layers/domain/repos/hospital_repo.dart';
+import 'package:doctor_booking_system_with_ai/core/network/network_info.dart';
 
-class HospitalRepoImpl implements HospitalRepo{
+class HospitalRepoImpl implements HospitalRepo {
+  final HospitalRemoteDataSource _remoteDataSource;
+  final NetworkInfo _networkInfo;
+
+  HospitalRepoImpl({
+    required HospitalRemoteDataSource remoteDataSource,
+    required NetworkInfo networkInfo,
+  }) : _remoteDataSource = remoteDataSource,
+       _networkInfo = networkInfo;
+
   @override
-  Future<Either<Failure, List<Hospital>>> getHospitals() {
-    // TODO: implement getHospitals
-    throw UnimplementedError();
+  Future<Either<Failure, List<Hospital>>> getHospitals() async {
+    try {
+      final result = await _remoteDataSource.getHospitals();
+
+      if (result.isEmpty) {
+        return Left(Failure('لم يتم العثور على المستشفيات'));
+      }
+
+      return Right(result);
+    } catch (error) {
+      return Left(Failure(error.toString()));
+    }
   }
 }
