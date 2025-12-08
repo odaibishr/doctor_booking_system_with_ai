8da74fa1938d91ca13e5d2304750d94e361af59a
Odai Bishr <odaibishr@gmail.com>
2025-12-09 01:53:06 +0300

    implements cach and get function in hospital local data source

--- lib/core/layers/data/datasources/hospital_local_data_source.dart
+++ lib/core/layers/data/datasources/hospital_local_data_source.dart
@@ -1,6 +1,27 @@
 import 'package:doctor_booking_system_with_ai/core/layers/domain/entities/hospital.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 abstract class HospitalLocalDataSource {
   Future<List<Hospital>> getCachedHospitals();
+  Future<void> cachedHospitals(List<Hospital> hospitals);
 }
 
+class HospitalLocalDataSourceImpl implements HospitalLocalDataSource {
+  final Box<Hospital> _hospitalBox;
+
+  HospitalLocalDataSourceImpl(this._hospitalBox);
+
+  @override
+  Future<List<Hospital>> getCachedHospitals() async {
+    return _hospitalBox.values.toList();
+  }
+
+  @override
+  Future<void> cachedHospitals(List<Hospital> hospitals) async {
+    await _hospitalBox.clear();
+
+    for (final hospital in hospitals) {
+      await _hospitalBox.put(hospital.id, hospital);
+    }
+  }
+}
