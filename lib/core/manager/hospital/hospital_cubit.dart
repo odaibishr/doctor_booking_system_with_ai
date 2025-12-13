d11997a8c6ebe70db155217bfef035567269066a
Odai Bishr <odaibishr@gmail.com>
2025-12-13 17:23:20 +0300

    Add Hospital cubit

--- lib/core/manager/hospital/hospital_cubit.dart
+++ lib/core/manager/hospital/hospital_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:doctor_booking_system_with_ai/core/layers/domain/entities/hospital.dart';
+import 'package:doctor_booking_system_with_ai/core/layers/domain/usecases/get_hospitals_use_case.dart';
 import 'package:meta/meta.dart';
 
 part 'hospital_state.dart';
 
 class HospitalCubit extends Cubit<HospitalState> {
-  HospitalCubit() : super(HospitalInitial());
+  final GetHospitalsUseCase getHospitalsUseCase;
+  HospitalCubit(this.getHospitalsUseCase) : super(HospitalInitial());
+
+  Future<void> getHospitals() async {
+    emit(HospitalLoading());
+    try {
+      final result = await getHospitalsUseCase();
+
+      result.fold(
+        (failure) => emit(HospitalError(failure.errorMessage)),
+        (hospitals) => emit(HospitalLoadded(hospitals)),
+      );
+    } catch (error) {
+      emit(HospitalError(error.toString()));
+    }
+  }
 }
