ef26270f281df0d8544309d44b4e019c0f13fd10
Odai Bishr <odaibishr@gmail.com>
2025-12-16 20:37:51 +0300

    Refactor review repo

--- lib/core/layers/domain/repos/review_repo.dart
+++ lib/core/layers/domain/repos/review_repo.dart
@@ -3,6 +3,10 @@ import 'package:doctor_booking_system_with_ai/core/errors/failure.dart';
 import 'package:doctor_booking_system_with_ai/core/layers/domain/entities/review.dart';
 
 abstract class ReviewRepo {
-  Future<Either<Failure, Review>> createReview(Review review);
+  Future<Either<Failure, Review>> createReview({
+    required int doctorId,
+    required int rating,
+    required String comment,
+  });
   Future<Either<Failure, List<Review>>> getDoctorReviews(int docotrId);
 }
