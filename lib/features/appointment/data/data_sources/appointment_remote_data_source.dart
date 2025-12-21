1ad6f6bf33cd8b3d0e6d41c444ddc180e8a980a1
Odai Bishr <odaibishr@gmail.com>
2025-12-22 00:00:45 +0300

    feat: implement appointmetn remote data source

--- lib/features/appointment/data/data_sources/appointment_remote_data_source.dart
+++ lib/features/appointment/data/data_sources/appointment_remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:doctor_booking_system_with_ai/core/database/api/dio_consumer.dart';
+import 'package:doctor_booking_system_with_ai/features/appointment/data/models/appointment_model.dart';
 import 'package:doctor_booking_system_with_ai/features/appointment/domain/entities/appointment.dart';
 
 abstract class AppointmentRemoteDataSource {
@@ -9,4 +11,33 @@ abstract class AppointmentRemoteDataSource {
     required String time,
     required String statue,
   });
+}
+
+class AppointmentRemoteDataSourceImpl implements AppointmentRemoteDataSource {
+  final DioConsumer dioConsumer;
+
+  AppointmentRemoteDataSourceImpl(this.dioConsumer);
+
+  @override
+  Future<Appointment> createAppointment({
+    required int doctorId,
+    required int doctorScheduleId,
+    required int transitionId,
+    required String date,
+    required String time,
+    required String statue,
+  }) async {
+    final response = await dioConsumer.post(
+      'appointment/createAppointment',
+      data: {
+        'doctor_id': doctorId,
+        'doctor_schedule_id': doctorScheduleId,
+        'transition_id': transitionId,
+        'date': date,
+        'time': time,
+        'statue': statue,
+      },
+    );
+    return AppointmentModel.fromMap(response['data']);
+  }
 }
