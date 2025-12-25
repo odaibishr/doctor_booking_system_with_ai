5f9c498deac9f9cf722f192cb0c20298a259610e
Odai Bishr <odaibishr@gmail.com>
2025-12-25 22:48:50 +0300

    feat: generate review adapter

--- lib/core/layers/domain/entities/review.dart
+++ lib/core/layers/domain/entities/review.dart
@@ -1,11 +1,20 @@
 import 'package:doctor_booking_system_with_ai/features/auth/domain/entities/user.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+part 'review.g.dart';
 
+@HiveType(typeId: 7)
 class Review {
+  @HiveField(0)
   final int id;
+  @HiveField(1)
   final int doctorId;
+  @HiveField(2)
   final int userId;
+  @HiveField(3)
   final String comment;
+  @HiveField(4)
   final int rating;
+  @HiveField(5)
   final User? user;
 
   Review({
---
+++ lib/core/layers/domain/entities/review.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'review.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ReviewAdapter extends TypeAdapter<Review> {
+  @override
+  final int typeId = 7;
+
+  @override
+  Review read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Review(
+      id: fields[0] as int,
+      doctorId: fields[1] as int,
+      userId: fields[2] as int,
+      comment: fields[3] as String,
+      rating: fields[4] as int,
+      user: fields[5] as User?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Review obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.doctorId)
+      ..writeByte(2)
+      ..write(obj.userId)
+      ..writeByte(3)
+      ..write(obj.comment)
+      ..writeByte(4)
+      ..write(obj.rating)
+      ..writeByte(5)
+      ..write(obj.user);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ReviewAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
