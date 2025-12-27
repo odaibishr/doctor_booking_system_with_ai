551943fe2dc7dade2305c335e8fc35f382713630
Odai Bishr <odaibishr@gmail.com>
2025-12-27 21:24:33 +0300

    style(jaib_dailog): change background in dark mode

--- lib/features/payment/presentation/widget/jaib_dialog_content.dart
+++ lib/features/payment/presentation/widget/jaib_dialog_content.dart
@@ -7,9 +7,7 @@ import 'package:flutter/widgets.dart';
 import 'package:svg_flutter/svg.dart';
 
 class JaibDialogContent extends StatefulWidget {
-  const JaibDialogContent({
-    super.key,
-  });
+  const JaibDialogContent({super.key});
 
   @override
   State<JaibDialogContent> createState() => _JaibDialogContentState();
@@ -17,7 +15,7 @@ class JaibDialogContent extends StatefulWidget {
 
 class _JaibDialogContentState extends State<JaibDialogContent> {
   bool compeletePayment = false;
- void showBottomSheetNow() {
+  void showBottomSheetNow() {
     setState(() {
       compeletePayment = true;
     });
@@ -27,39 +25,28 @@ class _JaibDialogContentState extends State<JaibDialogContent> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: AppColors.gray200,
-        border: Border.all(color: Color.fromARGB(255, 171, 25, 32),width: 1),
+        color: context.gray200Color,
+        border: Border.all(color: Color.fromARGB(255, 171, 25, 32), width: 1),
         borderRadius: BorderRadius.all(Radius.circular(20)),
       ),
-      
-      padding: EdgeInsets.symmetric(
-        horizontal: 22,
-        vertical: 10,
-      ),
-      
+
+      padding: EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           SvgPicture.asset(
-           'assets/images/jaib.svg' ,
+            'assets/images/jaib.svg',
             width: 70,
             height: 70,
-            colorFilter: ColorFilter.mode(
-              Color(0xffdb232d),
-              BlendMode.srcIn,
-            ),
+            colorFilter: ColorFilter.mode(Color(0xffdb232d), BlendMode.srcIn),
           ),
-          SizedBox(height: 5,),
+          SizedBox(height: 5),
           Text(
             'ادخل كود الشراء',
-            style: FontStyles.headLine4.copyWith(
-              color: Color(0xffdb232d),
-            ),
+            style: FontStyles.headLine4.copyWith(color: Color(0xffdb232d)),
           ),
-          SizedBox(
-            height:
-                MediaQuery.of(context).size.height * 0.03,
-          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.03),
           MainInputField(
             hintText: '                   ********* ',
             leftIconPath: '',
@@ -68,22 +55,17 @@ class _JaibDialogContentState extends State<JaibDialogContent> {
             isShowLeftIcon: false,
             border: Color(0xffdb232d),
           ),
-          SizedBox(
-            height:
-                MediaQuery.of(context).size.height * 0.04,
+          SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+          MainButton(
+            text: 'تاكيد',
+            color: Color(0xffdb232d),
+            onTap: () {
+              PaymentBottomSheet();
+              showBottomSheetNow();
+              Future.delayed(Duration(milliseconds: 300), () {});
+              Navigator.pop(context);
+            },
           ),
-          MainButton(text: 'تاكيد',color: Color(0xffdb232d),
-          onTap: (){
-            PaymentBottomSheet();
-          showBottomSheetNow();
-             Future.delayed(Duration(milliseconds: 300), () {
-              
-            
-            });
-            Navigator.pop(context); 
-           
-           
-          },),
         ],
       ),
     );
