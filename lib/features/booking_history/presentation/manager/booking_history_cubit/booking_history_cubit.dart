a220561e5cdb8d64a7b53761daa39097338a4b57
Odai Bishr <odaibishr@gmail.com>
2026-01-10 21:19:32 +0300

    feat(booking_history): add cancellation cubit

--- lib/features/booking_history/presentation/manager/booking_history_cubit/booking_history_cubit.dart
+++ lib/features/booking_history/presentation/manager/booking_history_cubit/booking_history_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:doctor_booking_system_with_ai/features/booking_history/domain/entities/booking.dart';
+import 'package:doctor_booking_system_with_ai/features/booking_history/domain/usecases/cancel_appointment_use_case.dart';
 import 'package:doctor_booking_system_with_ai/features/booking_history/domain/usecases/get_booking_history_use_case.dart';
 import 'package:flutter/foundation.dart';
 
@@ -7,10 +8,13 @@ part 'booking_history_state.dart';
 
 class BookingHistoryCubit extends Cubit<BookingHistoryState> {
   final GetBookingHistoryUseCase getBookingHistoryUseCase;
+  final CancelAppointmentUseCase cancelAppointmentUseCase;
   int _requestId = 0;
 
-  BookingHistoryCubit(this.getBookingHistoryUseCase)
-      : super(BookingHistoryInitial());
+  BookingHistoryCubit(
+    this.getBookingHistoryUseCase,
+    this.cancelAppointmentUseCase,
+  ) : super(BookingHistoryInitial());
 
   Future<void> fetchBookingHistory() async {
     final requestId = ++_requestId;
@@ -35,6 +39,19 @@ class BookingHistoryCubit extends Cubit<BookingHistoryState> {
       if (isClosed || requestId != _requestId) return;
       emit(BookingHistoryError(error.toString()));
     }
+  }
+
+  Future<void> cancelAppointment(int appointmentId, String reason) async {
+    emit(CancelAppointmentLoading());
+
+    final result = await cancelAppointmentUseCase(
+      CancelAppointmentParams(appointmentId, reason),
+    );
+
+    result.fold(
+      (failure) => emit(CancelAppointmentError(failure.errorMessage)),
+      (_) => emit(CancelAppointmentSuccess()),
+    );
   }
 
   @override
