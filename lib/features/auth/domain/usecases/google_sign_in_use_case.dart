30a71bf883d4030de02b2b3027d9b28a3d7fbf32
Odai Bishr <odaibishr@gmail.com>
2026-01-20 22:35:23 +0300

    feat: implement Google Sign-In service

--- lib/features/auth/domain/usecases/google_sign_in_use_case.dart
+++ lib/features/auth/domain/usecases/google_sign_in_use_case.dart
@@ -1,40 +1,14 @@
 import 'package:dartz/dartz.dart';
 import 'package:doctor_booking_system_with_ai/core/errors/failure.dart';
-import 'package:doctor_booking_system_with_ai/core/usecases/usecase.dart';
 import 'package:doctor_booking_system_with_ai/features/auth/domain/entities/user.dart';
 import 'package:doctor_booking_system_with_ai/features/auth/domain/repos/auth_repo.dart';
 
-class GoogleSignInUseCase extends Usecase<User, GoogleSignInParams> {
+class GoogleSignInUseCase {
   final AuthRepo authRepo;
 
   GoogleSignInUseCase(this.authRepo);
-  @override
-  Future<Either<Failure, User>> call([GoogleSignInParams? params]) async {
-    return await authRepo.signInWithGoogle(
-      name: params!.name,
-      email: params.email,
-      googleId: params.googleId,
-      idToken: params.idToken,
-      photoUrl: params.photoUrl,
-      fcmToken: params.fcmToken,
-    );
+
+  Future<Either<Failure, User>> call() async {
+    return await authRepo.signInWithGoogle();
   }
-}
-
-class GoogleSignInParams {
-  final String name;
-  final String email;
-  final String googleId;
-  final String idToken;
-  final String? photoUrl;
-  final String? fcmToken;
-
-  GoogleSignInParams({
-    required this.name,
-    required this.email,
-    required this.googleId,
-    required this.idToken,
-    this.photoUrl,
-    this.fcmToken,
-  });
 }
