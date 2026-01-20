9a2255aa00ebe95d8b4806f86c7a8db2fece84e0
Odai Bishr <odaibishr@gmail.com>
2026-01-20 22:51:35 +0300

    fix: end points

--- lib/core/database/api/end_points.dart
+++ lib/core/database/api/end_points.dart
@@ -1,19 +1,21 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class EndPoints {
   static String ip = dotenv.env['APP_IP'] ?? '127.0.0.1';
   static String port = dotenv.env['APP_PORT'] ?? '8000';
 
   static String get baseUrl {
     if (ip.startsWith('http')) {
-       // ... بقية الكود الذكي ...
-       return "$cleanIp/api/v1/";
+      final cleanIp = ip.endsWith('/') ? ip.substring(0, ip.length - 1) : ip;
+      return "$cleanIp/api/v1/";
     }
     return "http://$ip:$port/api/v1/";
   }
 
   static String get photoUrl {
     if (ip.startsWith('http')) {
-       // ... بقية الكود الذكي ...
-       return "$cleanIp/storage";
+      final cleanIp = ip.endsWith('/') ? ip.substring(0, ip.length - 1) : ip;
+      return "$cleanIp/storage";
     }
     return "http://$ip:$port/storage";
   }
