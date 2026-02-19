bf5c2f4a69aa704c887d7aeaa511c9f777456f1c
Odai Bishr <odaibishr@gmail.com>
2026-02-20 02:59:09 +0300

    fix: remove scroll animations from Booking and Search lists for smoother UX

--- lib/features/search/presentation/widgets/search_doctor_list_view.dart
+++ lib/features/search/presentation/widgets/search_doctor_list_view.dart
@@ -1,6 +1,6 @@
 import 'package:doctor_booking_system_with_ai/core/layers/domain/entities/doctor.dart';
 import 'package:doctor_booking_system_with_ai/core/utils/responsive.dart';
-import 'package:doctor_booking_system_with_ai/core/widgets/animated_widgets.dart';
+
 import 'package:flutter/material.dart';
 import 'package:doctor_booking_system_with_ai/core/widgets/doctor_card_horizontail.dart';
 
@@ -20,12 +20,7 @@ class SearchDoctorListView extends StatelessWidget {
         ),
         itemCount: doctors.length,
         itemBuilder: (context, index) {
-          return AnimatedListItem(
-            index: index,
-            delay: const Duration(milliseconds: 60),
-            animationType: AnimationType.fadeSlideUp,
-            child: DoctorCardHorizontail(doctor: doctors[index]),
-          );
+          return DoctorCardHorizontail(doctor: doctors[index]);
         },
       );
     }
@@ -33,12 +28,7 @@ class SearchDoctorListView extends StatelessWidget {
       scrollDirection: Axis.vertical,
       itemCount: doctors.length,
       itemBuilder: (context, index) {
-        return AnimatedListItem(
-          index: index,
-          delay: const Duration(milliseconds: 80),
-          animationType: AnimationType.fadeSlideUp,
-          child: DoctorCardHorizontail(doctor: doctors[index]),
-        );
+        return DoctorCardHorizontail(doctor: doctors[index]);
       },
     );
   }
