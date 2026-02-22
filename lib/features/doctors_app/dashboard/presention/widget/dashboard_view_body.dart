72e1da2f936afa8422efc8398c79f72412d98842
eng-ahmed0 <ahmedalariqie9@gmail.com>
2026-02-22 04:20:55 +0300

    edites

--- lib/features/doctors_app/dashboard/presention/widget/dashboard_view_body.dart
+++ lib/features/doctors_app/dashboard/presention/widget/dashboard_view_body.dart
@@ -29,28 +29,28 @@ class DashboardViewBody extends StatelessWidget {
                     ],
                   ),
                 ),
-                SizedBox(height: 16,),
+                SizedBox(height: 25,),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    DoctorCard(CardHeight: 110, Cardwidth: 160, CardIcon: Icons.person, CardTitle: '  عدد المرضى', CardContent: '(20)',),
-                    DoctorCard(CardHeight: 110, Cardwidth: 160, CardIcon: Icons.timelapse, CardTitle: '      عودة', CardContent: '(20)',),
+                    DoctorCard(CardHeight: 100, Cardwidth: 160, CardIcon: Icons.person, CardTitle: '  عدد المرضى', CardContent: '(20)',),
+                    DoctorCard(CardHeight: 100, Cardwidth: 160, CardIcon: Icons.timelapse, CardTitle: '      عودة', CardContent: '(20)',),
                   ],
                 ),
                 SizedBox(height: 14,),
                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    DoctorCard(CardHeight: 110, Cardwidth: 160, CardIcon: Icons.local_hospital_rounded, CardTitle: '  المستشفى', CardContent: '(سيبلاس)',),
-                    DoctorCard(CardHeight: 110, Cardwidth: 160, CardIcon:Icons.cancel, CardTitle:'      ملغية', CardContent: '(30)',),
+                    DoctorCard(CardHeight: 100, Cardwidth: 160, CardIcon: Icons.local_hospital_rounded, CardTitle: '  المستشفى', CardContent: '(سيبلاس)',),
+                    DoctorCard(CardHeight: 100, Cardwidth: 160, CardIcon:Icons.cancel, CardTitle:'      ملغية', CardContent: '(30)',),
                   ],
                 ),
                 SizedBox(height: 14,),
                  Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    DoctorCard(CardHeight: 110, Cardwidth: 160, CardIcon:Icons.timer_rounded, CardTitle:' ساعات الدوام', CardContent: '8:00-12:00',),
-                    DoctorCard(CardHeight: 110, Cardwidth: 160, CardIcon:Icons.holiday_village_rounded, CardTitle:'    أيام الاجازة', CardContent: 'Sat,Mon...',),
+                    DoctorCard(CardHeight: 100, Cardwidth: 160, CardIcon:Icons.timer_rounded, CardTitle:' ساعات الدوام', CardContent: '8:00-12:00',),
+                    DoctorCard(CardHeight: 100, Cardwidth: 160, CardIcon:Icons.holiday_village_rounded, CardTitle:'    أيام الاجازة', CardContent: 'Sat,Mon...',),
                   ],
                 )
                 
