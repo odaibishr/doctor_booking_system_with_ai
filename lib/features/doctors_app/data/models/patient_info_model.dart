34077bf81915f89917f20192a07048e3d0bda8d6
Odai Bishr <odaibishr@gmail.com>
2026-02-27 16:30:23 +0300

    feat: add PatientInfoModel for data mapping and serialization

---
+++ lib/features/doctors_app/data/models/patient_info_model.dart
@@ -0,0 +1,35 @@
+import 'package:doctor_booking_system_with_ai/core/utils/parse_helpers.dart';
+import 'package:doctor_booking_system_with_ai/features/doctors_app/domain/entities/patient_info.dart';
+
+class PatientInfoModel extends PatientInfo {
+  PatientInfoModel({
+    required super.id,
+    required super.name,
+    required super.email,
+    super.phone,
+    super.profileImage,
+    super.gender,
+  });
+
+  factory PatientInfoModel.fromMap(Map<String, dynamic> map) {
+    return PatientInfoModel(
+      id: parseToInt(map['id']),
+      name: (map['name'] ?? '').toString(),
+      email: (map['email'] ?? '').toString(),
+      phone: map['phone']?.toString(),
+      profileImage: map['profile_image']?.toString(),
+      gender: map['gender']?.toString(),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'phone': phone,
+      'profile_image': profileImage,
+      'gender': gender,
+    };
+  }
+}
