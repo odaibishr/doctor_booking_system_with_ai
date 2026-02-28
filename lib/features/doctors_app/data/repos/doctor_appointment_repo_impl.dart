20ef201a04480a354c946e916b25688ac371836e
Odai Bishr <odaibishr@gmail.com>
2026-02-28 23:34:41 +0300

    feat: implement getAppointments in DoctorAppointmentRepoImpl

--- lib/features/doctors_app/data/repos/doctor_appointment_repo_impl.dart
+++ lib/features/doctors_app/data/repos/doctor_appointment_repo_impl.dart
@@ -1,5 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:doctor_booking_system_with_ai/core/errors/failure.dart';
 import 'package:doctor_booking_system_with_ai/core/network/network_info.dart';
 import 'package:doctor_booking_system_with_ai/features/doctors_app/data/data_sources/doctor_appointment_remote_data_source.dart';
+import 'package:doctor_booking_system_with_ai/features/doctors_app/domain/entities/doctor_appointment.dart';
 import 'package:doctor_booking_system_with_ai/features/doctors_app/domain/repos/doctor_appointment_repo.dart';
 
 class DoctorAppointmentRepoImpl implements DoctorAppointmentRepo {
@@ -7,4 +10,24 @@ class DoctorAppointmentRepoImpl implements DoctorAppointmentRepo {
   final NetworkInfo networkInfo;
 
   DoctorAppointmentRepoImpl(this.remoteDataSource, this.networkInfo);
+
+  @override
+  Future<Either<Failure, List<DoctorAppointment>>> getAppointments({
+    String? status,
+    String? date,
+  }) async {
+    try {
+      if (!await networkInfo.isConnected) {
+        return Left(Failure('لايوجد اتصال بالانترنت'));
+      }
+
+      final result = await remoteDataSource.getAppointments(
+        status: status,
+        date: date,
+      );
+      return Right(result);
+    } catch (error) {
+      return Left(Failure(error.toString()));
+    }
+  }
 }
