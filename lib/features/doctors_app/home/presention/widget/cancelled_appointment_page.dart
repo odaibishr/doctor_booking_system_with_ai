f0ba23d13c21f5abbae13299396c05bc8e0cd6e3
Odai Bishr <odaibishr@gmail.com>
2026-03-01 01:28:58 +0300

    feat: format appointment date in CancelledAppointmentPage

--- lib/features/doctors_app/home/presention/widget/cancelled_appointment_page.dart
+++ lib/features/doctors_app/home/presention/widget/cancelled_appointment_page.dart
@@ -4,6 +4,7 @@ import 'package:doctor_booking_system_with_ai/features/doctors_app/home/presenti
 import 'package:doctor_booking_system_with_ai/features/doctors_app/managers/appointments/doctor_appointments_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 class CancelledAppointmentPage extends StatelessWidget {
   const CancelledAppointmentPage({super.key});
@@ -36,7 +37,15 @@ class CancelledAppointmentPage extends StatelessWidget {
                   patientImage: appointment.patientInfo?.profileImage ?? '',
                   time:
                       '${appointment.scheduleInfo?.startTime ?? ''} - ${appointment.scheduleInfo?.endTime ?? ''}',
-                  date: appointment.date,
+                  date: () {
+                    try {
+                      return DateFormat.yMMMMd(
+                        'ar',
+                      ).format(DateTime.parse(appointment.date));
+                    } catch (_) {
+                      return appointment.date;
+                    }
+                  }(),
                   bookingNumber: '${appointment.id}',
                   location: appointment.cancellationReason ?? '',
                   showActions: false,
