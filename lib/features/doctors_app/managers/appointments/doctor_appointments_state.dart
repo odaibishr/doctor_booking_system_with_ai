cf1e8bca26c41f2d318fd59531b36adb7e76581a
Odai Bishr <odaibishr@gmail.com>
2026-03-01 00:30:16 +0300

    feat: add loading, loaded, error, and status update states to DoctorAppointmentsState

--- lib/features/doctors_app/managers/appointments/doctor_appointments_state.dart
+++ lib/features/doctors_app/managers/appointments/doctor_appointments_state.dart
@@ -8,3 +8,32 @@ sealed class DoctorAppointmentsState extends Equatable {
 }
 
 final class DoctorAppointmentsInitial extends DoctorAppointmentsState {}
+
+final class DoctorAppointmentsLoading extends DoctorAppointmentsState {}
+
+final class DoctorAppointmentsLoaded extends DoctorAppointmentsState {
+  final List<DoctorAppointment> appointments;
+
+  const DoctorAppointmentsLoaded(this.appointments);
+}
+
+final class DoctorAppointmentsError extends DoctorAppointmentsState {
+  final String message;
+
+  const DoctorAppointmentsError(this.message);
+}
+
+final class DoctorAppointmentStatusUpdating extends DoctorAppointmentsState {}
+
+final class DoctorAppointmentStatusUpdated extends DoctorAppointmentsState {
+  final DoctorAppointment appointment;
+
+  const DoctorAppointmentStatusUpdated(this.appointment);
+}
+
+final class DoctorAppointmentStatusUpdatedError
+    extends DoctorAppointmentsState {
+  final String message;
+
+  const DoctorAppointmentStatusUpdatedError(this.message);
+}
