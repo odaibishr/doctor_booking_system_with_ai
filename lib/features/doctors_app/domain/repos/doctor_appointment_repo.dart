547f828db8ca2b151021e46c351caaff5f94b264
Odai Bishr <odaibishr@gmail.com>
2026-02-28 23:20:06 +0300

    feat: add DoctorAppointmentRepo abstract class

---
+++ lib/features/doctors_app/domain/repos/doctor_appointment_repo.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:doctor_booking_system_with_ai/core/errors/failure.dart';
+import 'package:doctor_booking_system_with_ai/features/doctors_app/domain/entities/doctor_appointment.dart';
+
+abstract class DoctorAppointmentRepo {
+  
+}
