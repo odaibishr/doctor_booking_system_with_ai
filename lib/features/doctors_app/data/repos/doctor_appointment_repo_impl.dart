1eb2063c488119f8cae31d1bfc74d83e199a3d5d
Odai Bishr <odaibishr@gmail.com>
2026-02-28 23:53:23 +0300

    feat: implement appointment management methods in DoctorAppointmentRepoImpl

--- lib/features/doctors_app/data/repos/doctor_appointment_repo_impl.dart
+++ lib/features/doctors_app/data/repos/doctor_appointment_repo_impl.dart
@@ -30,4 +30,87 @@ class DoctorAppointmentRepoImpl implements DoctorAppointmentRepo {
       return Left(Failure(error.toString()));
     }
   }
+
+  @override
+  Future<Either<Failure, DoctorAppointment>> getAppointmentDetails(
+    int id,
+  ) async {
+    try {
+      if (!await networkInfo.isConnected) {
+        return Left(Failure('لايوجد اتصال بالانترنت'));
+      }
+
+      final result = await remoteDataSource.getAppointmentDetails(id);
+      return Right(result);
+    } catch (error) {
+      return Left(Failure(error.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<DoctorAppointment>>>
+  getHistoryAppointments() async {
+    try {
+      if (!await networkInfo.isConnected) {
+        return Left(Failure('لايوجد اتصال بالانترنت'));
+      }
+
+      final result = await remoteDataSource.getHistoryAppointments();
+      return Right(result);
+    } catch (error) {
+      return Left(Failure(error.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<DoctorAppointment>>>
+  getTodayAppointments() async {
+    try {
+      if (!await networkInfo.isConnected) {
+        return Left(Failure('لايوجد اتصال بالانترنت'));
+      }
+
+      final result = await remoteDataSource.getTodayAppointments();
+      return Right(result);
+    } catch (error) {
+      return Left(Failure(error.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<DoctorAppointment>>>
+  getUpcomingAppointments() async {
+    try {
+      if (!await networkInfo.isConnected) {
+        return Left(Failure('لايوجد اتصال بالانترنت'));
+      }
+
+      final result = await remoteDataSource.getUpcomingAppointments();
+      return Right(result);
+    } catch (error) {
+      return Left(Failure(error.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, DoctorAppointment>> updateAppointmentStatus({
+    required int id,
+    required String status,
+    String? cancellationReason,
+  }) async {
+    try {
+      if (!await networkInfo.isConnected) {
+        return Left(Failure('لايوجد اتصال بالانترنت'));
+      }
+
+      final result = await remoteDataSource.updateAppointmentStatus(
+        id: id,
+        status: status,
+        cancellationReason: cancellationReason,
+      );
+      return Right(result);
+    } catch (error) {
+      return Left(Failure(error.toString()));
+    }
+  }
 }
