e36c44680685c9ab5b9e7046215e71f37018d9f5
Odai Bishr <odaibishr@gmail.com>
2026-02-28 23:20:20 +0300

    feat: add getAppointments to DoctorAppointmentRepo

--- lib/features/doctors_app/domain/repos/doctor_appointment_repo.dart
+++ lib/features/doctors_app/domain/repos/doctor_appointment_repo.dart
@@ -3,5 +3,8 @@ import 'package:doctor_booking_system_with_ai/core/errors/failure.dart';
 import 'package:doctor_booking_system_with_ai/features/doctors_app/domain/entities/doctor_appointment.dart';
 
 abstract class DoctorAppointmentRepo {
-  
+  Future<Either<Failure, List<DoctorAppointment>>> getAppointments({
+    String? status,
+    String? date,
+  });
 }
