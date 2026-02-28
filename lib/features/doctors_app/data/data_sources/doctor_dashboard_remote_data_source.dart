bb32ddad57ac48d222c7d1ba0c88362f9dd063d5
Odai Bishr <odaibishr@gmail.com>
2026-02-28 23:06:10 +0300

    feat: implement DoctorDashboardRemoteDataSourceImpl

--- lib/features/doctors_app/data/data_sources/doctor_dashboard_remote_data_source.dart
+++ lib/features/doctors_app/data/data_sources/doctor_dashboard_remote_data_source.dart
@@ -1,5 +1,24 @@
+import 'package:doctor_booking_system_with_ai/core/database/api/dio_consumer.dart';
+
 abstract class DoctorDashboardRemoteDataSource {
+  Future<Map<String, dynamic>> getDashboardStats({String filter = 'all'});
+}
+
+class DoctorDashboardRemoteDataSourceImpl
+    implements DoctorDashboardRemoteDataSource {
+  final DioConsumer dioConsumer;
+
+  DoctorDashboardRemoteDataSourceImpl(this.dioConsumer);
+
+  @override
   Future<Map<String, dynamic>> getDashboardStats({
     String filter = 'all',
-  });
+  }) async {
+    final response = await dioConsumer.get(
+      '/doctor/dashboard',
+      queryParameters: {'filter': filter},
+    );
+
+    return response['data'] as Map<String, dynamic>;
+  }
 }
