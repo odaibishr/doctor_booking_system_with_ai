bab6202e4b2c60837c286a1dad4c7b4fb2cd8003
Odai Bishr <odaibishr@gmail.com>
2026-03-01 01:24:52 +0300

    feat: provide DoctorAppointmentsCubit in HomePageView and fetch upcoming appointments

--- lib/features/doctors_app/home/presention/home_page_view.dart
+++ lib/features/doctors_app/home/presention/home_page_view.dart
@@ -1,11 +1,17 @@
 import 'package:doctor_booking_system_with_ai/features/doctors_app/home/presention/widget/home_page_view_body.dart';
+import 'package:doctor_booking_system_with_ai/features/doctors_app/managers/appointments/doctor_appointments_cubit.dart';
+import 'package:doctor_booking_system_with_ai/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePageView extends StatelessWidget {
   const HomePageView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: HomePageViewBody());
+    return BlocProvider(
+      create: (_) => serviceLocator<DoctorAppointmentsCubit>()..fetchUpcoming(),
+      child: const SafeArea(child: HomePageViewBody()),
+    );
   }
 }
