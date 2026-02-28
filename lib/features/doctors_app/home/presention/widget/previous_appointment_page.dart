0b28c926a377c912a209792b84754453b2214a93
Odai Bishr <odaibishr@gmail.com>
2026-03-01 01:26:13 +0300

    feat: implement DoctorAppointmentsCubit in PreviousAppointmentPage to display dynamic data

--- lib/features/doctors_app/home/presention/widget/previous_appointment_page.dart
+++ lib/features/doctors_app/home/presention/widget/previous_appointment_page.dart
@@ -1,24 +1,53 @@
-import 'package:doctor_booking_system_with_ai/features/doctors_app/home/presention/widget/doctor_information_card.dart';
+import 'package:doctor_booking_system_with_ai/core/utils/constant.dart';
+import 'package:doctor_booking_system_with_ai/core/widgets/custom_loader.dart';
+import 'package:doctor_booking_system_with_ai/features/doctors_app/home/presention/widget/dappointment_card.dart';
+import 'package:doctor_booking_system_with_ai/features/doctors_app/managers/appointments/doctor_appointments_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class PreviousAppointmentPage extends StatefulWidget {
+class PreviousAppointmentPage extends StatelessWidget {
   const PreviousAppointmentPage({super.key});
 
   @override
-  State<PreviousAppointmentPage> createState() => _PreviousAppointmentPageState();
-}
-
-class _PreviousAppointmentPageState extends State<PreviousAppointmentPage> {
-  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 20,left: 10,right: 10),
-      child: ListView(children: [
-        DoctorInformationCard(),
-        DoctorInformationCard(),
-        DoctorInformationCard(),
-      ],),
+    return BlocBuilder<DoctorAppointmentsCubit, DoctorAppointmentsState>(
+      builder: (context, state) {
+        if (state is DoctorAppointmentsLoading) {
+          return const Center(child: CustomLoader(loaderSize: kLoaderSize));
+        }
+
+        if (state is DoctorAppointmentsError) {
+          return Center(child: Text(state.message));
+        }
+
+        if (state is DoctorAppointmentsLoaded) {
+          final appointments = state.appointments;
+          if (appointments.isEmpty) {
+            return const Center(child: Text('لا توجد حجوزات سابقة'));
+          }
+          return Padding(
+            padding: const EdgeInsets.only(top: 20, left: 12, right: 12),
+            child: ListView.builder(
+              itemCount: appointments.length,
+              itemBuilder: (context, index) {
+                final appointment = appointments[index];
+                return DAppointmentCard(
+                  patientName: appointment.patientInfo?.name ?? 'مريض',
+                  patientImage: appointment.patientInfo?.profileImage ?? '',
+                  time:
+                      '${appointment.scheduleInfo?.startTime ?? ''} - ${appointment.scheduleInfo?.endTime ?? ''}',
+                  date: appointment.date,
+                  bookingNumber: '${appointment.id}',
+                  location: appointment.scheduleInfo?.dayName ?? '',
+                  showActions: false,
+                );
+              },
+            ),
+          );
+        }
+
+        return const SizedBox.shrink();
+      },
     );
   }
 }
-
