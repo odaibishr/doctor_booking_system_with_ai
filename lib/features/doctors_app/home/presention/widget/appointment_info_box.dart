ca0a87ec27c91117bebb74cafa9d967702bdd2d5
Odai Bishr <odaibishr@gmail.com>
2026-03-08 03:14:01 +0300

    feat: add AppointmentInfoBox widget

---
+++ lib/features/doctors_app/home/presention/widget/appointment_info_box.dart
@@ -0,0 +1,54 @@
+import 'package:doctor_booking_system_with_ai/core/styles/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppointmentInfoBox extends StatelessWidget {
+  const AppointmentInfoBox({
+    super.key,
+    required this.label,
+    required this.value,
+    required this.icon,
+  });
+
+  final String label;
+  final String value;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+      decoration: BoxDecoration(
+        color: context.gray200Color,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        children: [
+          Text(
+            label,
+            style: TextStyle(color: context.textTertiaryColor, fontSize: 10),
+          ),
+          const SizedBox(height: 4),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            textDirection: TextDirection.rtl,
+            children: [
+              Icon(icon, size: 14, color: context.primaryColor),
+              const SizedBox(width: 4),
+              Flexible(
+                child: Text(
+                  value,
+                  style: TextStyle(
+                    color: context.textPrimaryColor,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 12,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
