ad34bc0250abd164d0176a839d66ff449296b3dd
Odai Bishr <odaibishr@gmail.com>
2026-03-10 02:27:11 +0300

    feat: provide NotificationCubit to NotificationView using BlocProvider

--- lib/features/notification/presentation/notification_view.dart
+++ lib/features/notification/presentation/notification_view.dart
@@ -1,11 +1,17 @@
+import 'package:doctor_booking_system_with_ai/features/notification/presentation/manager/notification_cubit.dart';
 import 'package:doctor_booking_system_with_ai/features/notification/presentation/widgets/notification_view_body.dart';
+import 'package:doctor_booking_system_with_ai/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NotificationView extends StatelessWidget {
   const NotificationView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: NotificationViewBody()));
+    return BlocProvider(
+      create: (_) => serviceLocator<NotificationCubit>(),
+      child: const Scaffold(body: SafeArea(child: NotificationViewBody())),
+    );
   }
 }
