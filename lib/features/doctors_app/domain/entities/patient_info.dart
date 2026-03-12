47357cacc5ee8866e1dd492086dc19adca53ff5a
Odai Bishr <odaibishr@gmail.com>
2026-03-13 00:20:26 +0300

    refactor: add Hive annotations to PatientInfo entity

--- lib/features/doctors_app/domain/entities/patient_info.dart
+++ lib/features/doctors_app/domain/entities/patient_info.dart
@@ -1,9 +1,20 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+part 'patient_info.g.dart';
+
+@HiveType(typeId: 16)
 class PatientInfo {
+  @HiveField(0)
   final int id;
+  @HiveField(1)
   final String name;
+  @HiveField(2)
   final String email;
+  @HiveField(3)
   final String? phone;
+  @HiveField(4)
   final String? profileImage;
+  @HiveField(5)
   final String? gender;
 
   PatientInfo({
