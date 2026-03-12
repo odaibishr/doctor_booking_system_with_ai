0c94411a75a4b49ce3750366b116d2d07cfa54a6
Odai Bishr <odaibishr@gmail.com>
2026-03-12 23:45:32 +0300

    feat: add Hive annotations to DashboardStats entity

--- lib/features/doctors_app/domain/entities/dashboard_stats.dart
+++ lib/features/doctors_app/domain/entities/dashboard_stats.dart
@@ -1,19 +1,36 @@
 import 'package:doctor_booking_system_with_ai/core/layers/domain/entities/doctor_schedule.dart';
 import 'package:doctor_booking_system_with_ai/features/doctors_app/domain/entities/earnings_data.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
+part 'dashboard_stats.g.dart';
+
+@HiveType(typeId: 15)
 class DashboardStats {
+  @HiveField(0)
   final int todayAppointments;
+  @HiveField(1)
   final int upcomingAppointments;
+  @HiveField(2)
   final int completedAppointments;
+  @HiveField(3)
   final int cancelledAppointments;
+  @HiveField(4)
   final int totalPatients;
+  @HiveField(5)
   final int todayPatients;
+  @HiveField(6)
   final EarningsData earnings;
+  @HiveField(7)
   final int reviewsAvg;
+  @HiveField(8)
   final int reviewsCount;
+  @HiveField(9)
   final int waitlistCount;
+  @HiveField(10)
   final String? hospitalName;
+  @HiveField(11)
   final List<DoctorSchedule> workingHours;
+  @HiveField(12)
   final List<Map<String, dynamic>> daysOff;
 
   DashboardStats({
