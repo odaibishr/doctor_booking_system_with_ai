3f26f9f66d536b00049df7c7238febfc757976e9
Odai Bishr <odaibishr@gmail.com>
2026-03-14 23:41:14 +0300

    refactor: implement Hive storage for CachedQuery config

--- lib/core/cache/query_config.dart
+++ lib/core/cache/query_config.dart
@@ -1,16 +1,21 @@
 import 'package:cached_query_flutter/cached_query_flutter.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:doctor_booking_system_with_ai/core/services/hive_query_storage.dart';
 
 class AppQueryConfig {
   static const Duration defaultRefetchDuration = Duration(minutes: 5);
   static const Duration defaultCacheDuration = Duration(hours: 1);
 
-  static void init() {
+  static Future<void> init() async {
+    await Hive.initFlutter();
+    final queryBox = await Hive.openBox('query_cache');
+
     CachedQuery.instance.configFlutter(
       config: QueryConfigFlutter(
         refetchOnResume: true,
         refetchOnConnection: true,
       ),
-      storage: null,
+      storage: HiveQueryStorage(queryBox),
     );
   }
 
