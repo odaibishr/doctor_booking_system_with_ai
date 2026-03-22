2654eb45acd63807e6f5c7c721a2a7c097de8ec0
Odai Bishr <odaibishr@gmail.com>
2026-03-22 20:56:33 +0300

    refactor: replace SearchDoctorsBloc with SearchDoctorsCubit in service locator

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -68,8 +68,8 @@ import 'package:doctor_booking_system_with_ai/core/layers/domain/usecases/get_pr
 import 'package:doctor_booking_system_with_ai/features/profile/domain/repos/logout_repo.dart';
 import 'package:doctor_booking_system_with_ai/features/profile/domain/use_cases/logout_use_case.dart';
 import 'package:doctor_booking_system_with_ai/features/search/domain/usecases/search_doctors_use_case.dart';
-import 'package:doctor_booking_system_with_ai/features/search/presentation/manager/search_doctors_bloc/search_doctors_bloc.dart';
 import 'package:doctor_booking_system_with_ai/features/map/presentation/manager/map_bloc.dart';
+import 'package:doctor_booking_system_with_ai/features/search/presentation/manager/search_doctors_cubit/search_doctors_cubit.dart';
 import 'package:doctor_booking_system_with_ai/features/waitlist/data/datasources/waitlist_remote_data_source.dart';
 import 'package:doctor_booking_system_with_ai/features/waitlist/data/datasources/waitlist_remote_data_source_impl.dart';
 import 'package:doctor_booking_system_with_ai/features/waitlist/data/repos/waitlist_repo_impl.dart';
@@ -415,8 +415,8 @@ Future<void> init() async {
 
   serviceLocator.registerLazySingleton<SpecialtyCubit>(() => SpecialtyCubit());
 
-  serviceLocator.registerLazySingleton<SearchDoctorsBloc>(
-    () => SearchDoctorsBloc(
+  serviceLocator.registerLazySingleton<SearchDoctorsCubit>(
+    () => SearchDoctorsCubit(
       serviceLocator<SearchDoctorsUseCase>(),
       serviceLocator<GetDoctorsUseCase>(),
     ),
