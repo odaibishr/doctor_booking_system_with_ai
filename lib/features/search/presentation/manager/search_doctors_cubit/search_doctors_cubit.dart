ed385829d44d2981aba66330f55e179a315ecad4
Odai Bishr <odaibishr@gmail.com>
2026-03-22 21:54:18 +0300

    feat: add filtering capabilities to SearchDoctorsCubit

--- lib/features/search/presentation/manager/search_doctors_cubit/search_doctors_cubit.dart
+++ lib/features/search/presentation/manager/search_doctors_cubit/search_doctors_cubit.dart
@@ -12,8 +12,19 @@ part 'search_doctors_state.dart';
 class SearchQuery {
   final String query;
   final int? specialtyId;
+  final String? gender;
+  final double? minPrice;
+  final double? maxPrice;
+  final int? hospitalId;
 
-  SearchQuery({this.query = '', this.specialtyId});
+  SearchQuery({
+    this.query = '',
+    this.specialtyId,
+    this.gender,
+    this.minPrice,
+    this.maxPrice,
+    this.hospitalId,
+  });
 
   @override
   bool operator ==(Object other) =>
@@ -21,31 +32,78 @@ class SearchQuery {
       other is SearchQuery &&
           runtimeType == other.runtimeType &&
           query == other.query &&
-          specialtyId == other.specialtyId;
+          specialtyId == other.specialtyId &&
+          gender == other.gender &&
+          minPrice == other.minPrice &&
+          maxPrice == other.maxPrice &&
+          hospitalId == other.hospitalId;
 
   @override
-  int get hashCode => query.hashCode ^ specialtyId.hashCode;
+  int get hashCode =>
+      query.hashCode ^
+      specialtyId.hashCode ^
+      gender.hashCode ^
+      minPrice.hashCode ^
+      maxPrice.hashCode ^
+      hospitalId.hashCode;
 }
 
 class SearchDoctorsCubit extends Cubit<SearchDoctorsState> {
   final SearchDoctorsUseCase searchDoctorsUseCase;
   final GetDoctorsUseCase getDoctorsUseCase;
-  
+
   final _searchSubject = BehaviorSubject<SearchQuery>();
   late final StreamSubscription<void> _subscription;
-  
+
+  String _currentQuery = '';
+  int? _currentSpecialtyId;
+
   SearchDoctorsCubit(this.searchDoctorsUseCase, this.getDoctorsUseCase)
-    : super(SearchDoctorsInitial()) {
+      : super(SearchDoctorsInitial()) {
     _subscription = _searchSubject.stream
         .debounceTime(const Duration(milliseconds: 300))
         .switchMap((searchQuery) => _performSearch(searchQuery))
         .listen((_) {});
   }
 
+  void updateSearch(String query, [int? specialtyId]) {
+    _currentQuery = query;
+    _currentSpecialtyId = specialtyId ?? _currentSpecialtyId;
+    _searchSubject.add(SearchQuery(
+      query: query,
+      specialtyId: _currentSpecialtyId,
+    ));
+  }
+
+  void applyFilters({
+    String? gender,
+    double? minPrice,
+    double? maxPrice,
+    int? hospitalId,
+    int? specialtyId,
+  }) {
+    _currentSpecialtyId = specialtyId;
+    _searchSubject.add(SearchQuery(
+      query: _currentQuery,
+      specialtyId: specialtyId,
+      gender: gender,
+      minPrice: minPrice,
+      maxPrice: maxPrice,
+      hospitalId: hospitalId,
+    ));
+  }
+
   Stream<void> _performSearch(SearchQuery searchQuery) async* {
     emit(SearchDoctorloading());
 
-    if (searchQuery.query.trim().isEmpty && searchQuery.specialtyId == null) {
+    final bool hasFilters = searchQuery.gender != null ||
+        searchQuery.minPrice != null ||
+        searchQuery.maxPrice != null ||
+        searchQuery.hospitalId != null;
+
+    if (searchQuery.query.trim().isEmpty &&
+        searchQuery.specialtyId == null &&
+        !hasFilters) {
       final result = await getDoctorsUseCase(NoParams());
       result.fold(
         (failure) => emit(SearchDoctorsError(failure.errorMessage)),
@@ -56,6 +114,10 @@ class SearchDoctorsCubit extends Cubit<SearchDoctorsState> {
         SearchDoctorsUseCaseParams(
           query: searchQuery.query,
           specialtyId: searchQuery.specialtyId,
+          gender: searchQuery.gender,
+          minPrice: searchQuery.minPrice,
+          maxPrice: searchQuery.maxPrice,
+          hospitalId: searchQuery.hospitalId,
         ),
       );
       result.fold(
@@ -63,10 +125,6 @@ class SearchDoctorsCubit extends Cubit<SearchDoctorsState> {
         (doctors) => emit(SearchDoctorsLoaded(doctors)),
       );
     }
-  }
-
-  void updateSearch(String query, int? specialtyId) {
-    _searchSubject.add(SearchQuery(query: query, specialtyId: specialtyId));
   }
 
   @override
