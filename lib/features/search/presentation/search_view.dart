ec52f2a06c54ac57182fb9c53c2caa7621af2023
Odai Bishr <odaibishr@gmail.com>
2026-03-23 22:16:39 +0300

    feat: provide SearchDoctorsCubit in SearchView

--- lib/features/search/presentation/search_view.dart
+++ lib/features/search/presentation/search_view.dart
@@ -1,5 +1,8 @@
+import 'package:doctor_booking_system_with_ai/features/search/presentation/manager/search_doctors_cubit/search_doctors_cubit.dart';
 import 'package:doctor_booking_system_with_ai/features/search/presentation/widgets/search_view_body.dart';
+import 'package:doctor_booking_system_with_ai/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key, this.specialtyQuery});
@@ -7,6 +10,9 @@ class SearchView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SearchViewBody(specialtyQuery: specialtyQuery);
+    return BlocProvider(
+      create: (context) => serviceLocator<SearchDoctorsCubit>(),
+      child: SearchViewBody(specialtyQuery: specialtyQuery),
+    );
   }
 }
