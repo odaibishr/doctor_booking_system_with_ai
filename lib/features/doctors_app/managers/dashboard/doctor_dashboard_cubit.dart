b3534b66587dde94212b0e5a62f90e4d79edbb62
Odai Bishr <odaibishr@gmail.com>
2026-04-03 00:02:13 +0300

    feat: integrate PusherService into DoctorDashboardCubit for real-time updates

--- lib/features/doctors_app/managers/dashboard/doctor_dashboard_cubit.dart
+++ lib/features/doctors_app/managers/dashboard/doctor_dashboard_cubit.dart
@@ -5,16 +5,30 @@ import 'package:cached_query_flutter/cached_query_flutter.dart';
 import 'package:dartz/dartz.dart';
 import 'package:doctor_booking_system_with_ai/core/cache/cache_exports.dart';
 import 'package:doctor_booking_system_with_ai/core/errors/failure.dart';
+import 'package:doctor_booking_system_with_ai/core/services/pusher_service.dart';
 import 'package:doctor_booking_system_with_ai/features/doctors_app/domain/entities/dashboard_stats.dart';
 import 'package:equatable/equatable.dart';
 
 part 'doctor_dashboard_state.dart';
 
 class DoctorDashboardCubit extends Cubit<DoctorDashboardState> {
+  final PusherService _pusherService;
   Query<Either<Failure, DashboardStats>>? _dashboardQuery;
   StreamSubscription<QueryState<Either<Failure, DashboardStats>>>? _querySub;
+  StreamSubscription? _pusherSub;
 
-  DoctorDashboardCubit() : super(DoctorDashboardInitial());
+  DoctorDashboardCubit(this._pusherService) : super(DoctorDashboardInitial()) {
+    _listenToPusher();
+  }
+
+  void _listenToPusher() {
+    _pusherSub?.cancel();
+    _pusherSub = _pusherService.eventStream.listen((event) {
+      // Any appointment update should trigger a dashboard refresh
+      invalidateDoctorDashboardCache();
+      _dashboardQuery?.refetch();
+    });
+  }
 
   void _safeEmit(DoctorDashboardState state) {
     if (!isClosed) emit(state);
